cedb6b10e96e42bc1a8c3b75dd068f1e1669ef43
JurgenvanF <frae0004@hz.nl>
2026-02-02 18:22:16 +0100

    Profile Added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
 import "./Home.css";
+import logo from "../../assets/profilepicture.jpg";
 
 function Home() {
   return (
     <>
+      <img className="title__picture" src={logo} />
       <h1 className="title__home">Jurgen van Fraeijenhove</h1>
-      <p>Welkom op de home pagina!</p>
+      <p className="title__semititle">Front-end Designer & Developer 🚀</p>
+      <p className="title__description">
+        Hallo! 👋 Ik ben een developer met een passie voor design en houd ervan
+        om websites te maken en ontwerpen. Wanneer ik niet aan het coderen ben
+        speel ik graag een spelletje 🎮
+      </p>
     </>
   );
 }
